752919bdbbc54a89b09767641afb495b4c29e37c
FDhunter <faizaltriswanto@gmail.com>
2020-09-18 13:02:58 +0700

    v 2.0.1

--- core/api.dart
+++ core/api.dart
@@ -11,25 +11,28 @@ import 'env.dart';
 
 class Auth{
   Session session = new Session();
-  final username;
+  /// username Login Laravel OAuth >> .login();
+  final username; 
+  /// password Login Laravel OAuth >> .login();
   final password;
+  /// url name without root example 'get/user' >> .getUser();
   final name;
-  List getDataInt;
-  List getDataString;
-  List getDataBool;
-  List nameStringsession;
-  List dataStringsession;
-  List nameIntsession;
-  List dataIntsession;
-  List nameBoolsession;
-  List dataBoolsession;
-
-  Auth({Key key , this.nameStringsession, this.dataStringsession, this.nameIntsession, this.dataIntsession, this.nameBoolsession, this.dataBoolsession,this.name ,this.username, this.password , this.getDataInt , this.getDataBool , this.getDataString});
+  /// use it for get session in shared_preferences, example => getData:['user_id'] >> required;
+  List getData;
+  /// response data from API , example => responseData:['name','u_id'].
+  /// 
+  /// work with responseData
+  List responseData;
+  /// name for save session in shared_preferences, example => responseData:['name','u_id'] .
+  /// 
+  /// work with nameSession
+  List nameSession;
+  Auth({Key key , this.nameSession, this.responseData , this.getData , this.name ,@required this.username,@required this.password});
 
 
   login() async {
    try{
-    final sendlogin = await http.post(noapiurl+'oauth/token', body: {
+    final sendlogin = await http.post(noapiurl+'auth', body: {
         'grant_type': grantType,
         'client_id': clientId,
         'client_secret': clientsecret,
@@ -46,49 +49,44 @@ class Auth{
         } else if (getresponse['error'] == 'invalid_request') {
           Fluttertoast.showToast(msg:getresponse['hint']);
         } else if (getresponse['token_type'] == 'Bearer') {
+          print(getresponse['access_token']);
           session.saveString('access_token', getresponse['access_token']);
           session.saveString('token_type', getresponse['token_type']);
         }
       Fluttertoast.showToast(msg:'Token saved');
-      await getuser();
+      // await getUser();
       return print('success');
     }else{
       Fluttertoast.showToast(msg:'Error Code ${sendlogin.statusCode}');
       return print('failure');
     }
    } on SocketException catch (_) {
-      Fluttertoast.showToast(msg:'Connection Timed Out');
+      return Fluttertoast.showToast(msg:'Connection Timed Out');
     } on TimeoutException catch (_){
-      Fluttertoast.showToast(msg:'Request Timeout, try again');
+      return Fluttertoast.showToast(msg:'Request Timeout, try again');
     } catch (e) {
-      print(e.toString());
+      return print(e.toString());
     }
-    return Fluttertoast.showToast(msg:'Request Not Send to Url');
   }
 
   getUser() async {
-    dynamic getresponse = await RequestGet(name: name,customrequest: '').getdata();
+    dynamic getresponse = await Get(name: name,customrequest: '').request();
     // print(getresponse['cm_name']);
     if(getresponse.length > 0 ){
       
-      if(nameStringsession != null){  
-        for(var i = 0; i < nameStringsession.length ; i++){
-          session.saveString(nameStringsession[i], getresponse[dataStringsession != null ? dataStringsession[i] : nameStringsession[i]]);
+      if(nameSession != null){  
+        for(var i = 0; i < nameSession.length ; i++){
+          if(responseData[i]){
+            if(getresponse[responseData[i]] is String){
+              session.saveString(nameSession[i], getresponse[responseData[i]]);
+            }else if(getresponse[responseData[i]] is int){
+              session.saveInteger(nameSession[i], getresponse[responseData[i]]);
+            }else if(getresponse[responseData[i]] is bool){
+              session.saveBool(nameSession[i], getresponse[responseData[i]]);
+            }
+          }
         }
       }
-      
-      if(nameIntsession != null){
-        for(var i = 0; i < nameIntsession.length ; i++){
-          session.saveInteger(nameIntsession[i], getresponse[dataIntsession != null ? dataIntsession[i] : nameIntsession[i]]);
-        }
-      }
-
-      if(nameBoolsession != null){
-        for(var i = 0; i < nameBoolsession.length ; i++){
-          session.saveBool(nameBoolsession[i], getresponse[dataBoolsession != null ? dataBoolsession[i] : nameBoolsession[i]]);
-        }
-      }
-
       Fluttertoast.showToast(msg:'Login Success');
     }else{
     Fluttertoast.showToast(msg:'Profile Not Found');
@@ -97,30 +95,22 @@ class Auth{
 
   getsession() async {
     Map <String,dynamic> result = new Map();
-    if(getDataString != null){    
-      for(var i = 0 ; i < getDataString.length;i++){
-        result[getDataString[i]] = await session.getString(getDataString[i]);
+    if(getData != null){    
+      for(var i = 0 ; i < getData.length;i++){
+        if(result[getData[i]] is String){
+          result[getData[i]] = await session.getString(getData[i]);
+        }else if(result[getData[i]] is int){
+          result[getData[i]] = await session.getInteger(getData[i]);
+        }else if(result[getData[i]] is bool){
+          result[getData[i]] = await session.getBool(getData[i]);
+        }
       }
     }
-
-    if(getDataInt != null){
-      for(var i = 0 ; i < getDataInt.length;i++){
-        result[getDataInt[i]] = await session.getInteger(getDataInt[i]);
-      }
-    }
-
-    if(getDataBool != null){    
-      for(var i = 0 ; i < getDataBool.length;i++){
-        result[getDataBool[i]] = await session.getBool(getDataBool[i]);
-      }
-    }
-
-
     return result;
   }
 }
 
-class RequestGet{
+class Get{
   Session session = new Session();
   String name;
   String customrequest = '';
@@ -130,9 +120,9 @@ class RequestGet{
   String errorMessage;
   String successMessage;
   bool logResponse;
-  RequestGet({Key key , this.name , this.header , this.withbody , this.customrequest , this.customurl, this.errorMessage,this.logResponse, this.successMessage});
+  Get({Key key , this.name , this.header , this.withbody , this.customrequest , this.customurl, this.errorMessage,this.logResponse, this.successMessage});
 
-  getdata() async {
+  request() async {
 
     if(customurl != null && customurl != ''){
       url = customurl;
@@ -180,7 +170,7 @@ class RequestGet{
   }
 }
 
-class RequestPost{
+class Post{
   Session session = new Session();
   String name;
   dynamic body;
@@ -190,8 +180,8 @@ class RequestPost{
   bool logResponse;
   String customurl;
 
-  RequestPost({Key key , this.name , this.header,this.body , this.customurl,this.errorMessage,this.logResponse, this.successMessage});
-  sendrequest() async {
+  Post({Key key , this.name , this.header,this.body , this.customurl,this.errorMessage,this.logResponse, this.successMessage});
+  request() async {
     if(customurl != null && customurl != ''){
       url = customurl;
     }
