63331bc4d8303d4a5050f19f5e1df946426b4a49
FDhunter <faizaltriswanto@gmail.com>
2020-09-22 17:29:57 +0700

    Revert "Revert "revision V 2.0.2""
    This reverts commit ac9a081f90e74469fef11707bec5ccf72bc47880.

--- core/response.dart
+++ core/response.dart
@@ -1,54 +1,79 @@
 class Response{
   dynamic edited;
   dynamic total;
+  int loopLvl = 0;
   String tab = ' ';
-  String tablvl = '';
+  String tabLvl = '';
+  Map<dynamic,dynamic> thisarrlvl = <dynamic,dynamic>{};
   
   Response({this.total});
   
-  start(thisarr,lvl) async {
+  start(thisarr,lvl,arrayobject) async {
     print('please wait the process may take a long time...');
-    await replaceArr(thisarr,lvl);      
+    await replaceArr(thisarr,lvl,arrayobject);      
   }
   
-  replaceArr(thisarr,lvl){
-    if(thisarr is Map){
-      thisarr.forEach((key,value){
-        if(lvl == 1){
-          tablvl = '';
-        }else{          
-          tablvl += '$tab';
-        }
-      
-         print('$tablvl $key (Map)');
-         thisarr = value;
-         replaceArr(thisarr,lvl + 1);
-      });
-    }else if(thisarr is List){
-       if(lvl == 1){
-         tablvl = '';
-       }else{
-          tablvl += '$tab';
-       }
-      
-      
-      for(int i = 0; i < thisarr.length;i++){
-        if(thisarr[i] is String || thisarr[i] is int || thisarr[i] is bool)         {        
-          print('$tablvl ${thisarr[i]} (List)');
-        }else{
-          thisarr = thisarr[i];
-          replaceArr(thisarr,lvl + 1);
-        }
+  replaceArr(thisarr,lvl,arrayobject){
+   thisarrlvl[lvl] = thisarr;
+   bool arrobj = arrayobject;
+   
+   if(thisarrlvl[lvl] is Map){
+      if(lvl == 1){
+        tabLvl = '';
+      }if(arrobj == true){          
+      }else{
+        tabLvl += '$tab';
       }
+      thisarrlvl[lvl].forEach((key,value){
+         if(lvl == 1){
+           tabLvl = '';
+         }
+         if(value is String || value is int || value is bool){ 
+          print('$tabLvl $key : $value');
+         }else{
+           print('$tabLvl $key [');
+           thisarrlvl[lvl + 1] = value;
+           replaceArr(thisarrlvl[lvl + 1],lvl + 1,arrobj);
+         }
+      });
+    }else if(thisarrlvl[lvl] is List){
+      for(int i = 0; i < thisarrlvl[lvl].length;i++){
+         if(lvl == 1){
+           tabLvl = '';
+         }
+         if(i == 0){
+            tabLvl += ' ';
+         } 
+         if(thisarrlvl[lvl][i] is String || thisarrlvl[lvl][i] is int || thisarrlvl[lvl][i] is bool)     
+         {  
+         print('$tabLvl ${thisarrlvl[lvl][i]},');
+         if(i == thisarrlvl[lvl].length - 1){
+            print('$tabLvl ],');
+         }
+         }else{
+            if(thisarrlvl[lvl][i] is Map){
+               if(i > 0){
+                  print(' ');
+               }
+               arrobj = true;
+            }else{
+               arrobj = false;
+            }
+            thisarrlvl[lvl + 1] = thisarrlvl[lvl][i];
+            replaceArr(thisarrlvl[lvl + 1],lvl + 1,arrobj);
+            if(thisarrlvl[lvl][i] is Map){
+               if(i == thisarrlvl[lvl].length - 1){
+                  print('$tabLvl ],');
+               }
+            }
+         }
+      } 
+      arrobj = false;
     }else{
       if(lvl == 1){
-        tablvl = '';
-      }else{
-        tablvl += '$tab';
+        tabLvl = '';
       }
-      
-      print('$tablvl $thisarr (Var)');
-      
+      print('$tabLvl $thisarrlvl[lvl] (Var)');
     }
   }
 }
